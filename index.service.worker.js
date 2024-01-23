4906ca77df941848c4d4f676bb616af092d75099
FoxyTheThird <cardo.hedz123@gmail.com>
2024-01-23 17:20:09 -0500

    Didn't update?????????
    So I uploaded it again :3
    
    Anyways cloud

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1706047625|195977424434";
+const CACHE_VERSION = "1706048363|196714822546";
 const CACHE_PREFIX = "Web Games-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
