1213a23d501c5a5eb3063700e7f4690183efef74
FoxyTheThird <cardo.hedz123@gmail.com>
2024-03-20 23:08:08 -0400

    Trying to make it work fr

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1710989727|2303885890";
+const CACHE_VERSION = "1710990460|3037225185";
 const CACHE_PREFIX = "Web Games-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
